8e07180e37d7d628e0dce78eb9b168380bf9a820
tbrunogo@fastshop.com.br <brunoort@gmail.com>
2019-06-26 21:56:42 -0300

    chaging urls

--- client/src/components/MusiciansAdmin.js
+++ client/src/components/MusiciansAdmin.js
@@ -1,10 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import Musician from './Musician';
 import axios from "axios";
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 export default class MusiciansAdmin extends Component {
-
+  
   state = {
     newmusician: { 
       firstName: "",
@@ -13,20 +12,24 @@ export default class MusiciansAdmin extends Component {
     },
     musicians: []
   }
-
-
+  
+  
   outputHTML = "";
-
+  
   handleAddMusician = async(id, event) => {
     event.preventDefault();
     try {
-      this.http.get(`https://jarcakr0yd.execute-api.us-east-1.amazonaws.com/default/MusicanaApp`)
-      .map(res => {
-        console.log(res);
-      }).catch((err) => {
-        console.log(err);
-      });
-
+      
+      var url = `https://66my8mw9db.execute-api.us-east-1.amazonaws.com/default/MusicianApp`;
+      var requestify = require('requestify');
+      
+      requestify.get(url)
+      .then(function(response) {
+        // Get the response body (JSON parsed or jQuery object for XMLs)
+        console.log(response.getBody());
+      }
+      );
+      
       await axios.put(`/musician/${id}`, this.state.newmusician);
       this.setState({ musicians: [...this.state.musicians, this.state.newmusician] });
       this.setState({ newmusician: { "firstName": "", "lastName": "", "genre": "" }});
@@ -34,7 +37,7 @@ export default class MusiciansAdmin extends Component {
       console.log(err);
     }
   }
-
+  
   handleUpdateMusician = (id, name) => {
     // add call to AWS API Gateway update musician endpoint here
     const musicianToUpdate = [...this.state.musicians].find(musician => musician.id === id);
@@ -43,7 +46,7 @@ export default class MusiciansAdmin extends Component {
     updatedMusicians.push(musicianToUpdate);
     this.setState({musicians: updatedMusicians});
   }
-
+  
   handleDeleteMusician = async(id, event) => {
     // using firstName as the id in musicians state
     event.preventDefault();
@@ -55,7 +58,7 @@ export default class MusiciansAdmin extends Component {
       console.log(err);
     }
   }
-
+  
   fetchMusicians = async() => {
     try{
       const res = await axios.get('/musician/all');
@@ -67,87 +70,88 @@ export default class MusiciansAdmin extends Component {
       console.log(err);
     }
   }
-
+  
   onAddMusicianFirstNameChange = event => this.setState({ newmusician: { ...this.state.newmusician, "firstName": event.target.value } });
   onAddMusicianLastNameChange = event => this.setState({ newmusician: { ...this.state.newmusician, "lastName": event.target.value } });
   onAddMusicianGenreChange = event => this.setState({ newmusician: { ...this.state.newmusician, "genre": event.target.value } });
-
+  
   componentDidMount = () => {
     this.fetchMusicians();
   }
-
+  
   render() {
     return (
       <Fragment>
-        <section className="section">
-          <div className="container">
-            <p className="subtitle is-5">Add and remove musicians using the form below:</p>
-            <br />
-            <div className="columns">
-              <div className="column is-one-third">
-                <form onSubmit={event => this.handleAddMusician(this.state.newmusician.firstName.toLowerCase(), event)}>
-                  <div className="field">
-                    <div className="control">
-                      <input 
-                        className="input is-medium"
-                        type="text" 
-                        placeholder="Enter first name"
-                        value={this.state.newmusician.firstName}
-                        onChange={this.onAddMusicianFirstNameChange}
-                      />
-                    </div>
-                  </div>
-                  <div className="field">
-                    <div className="control">
-                      <input 
-                        className="input is-medium"
-                        type="text" 
-                        placeholder="Enter last name"
-                        value={this.state.newmusician.lastName}
-                        onChange={this.onAddMusicianLastNameChange}
-                      />
-                    </div>
-                  </div>
-                  <div className="field">
-                    <div className="control">
-                      <input 
-                        className="input is-medium"
-                        type="text" 
-                        placeholder="Enter genre"
-                        value={this.state.newmusician.genre}
-                        onChange={this.onAddMusicianGenreChange}
-                      />
-                    </div>
-                  </div>
-                  <div className="field">
-                    <div className="control">
-                      <button type="submit" className="button is-primary is-medium">
-                        Add musician
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <div className="column is-two-thirds">
-                {
-                  this.state.musicians.map((musician, index) =>
-                    <Musician
-                        isAdmin={true}
-                        handleUpdateMusician={this.handleUpdateMusician}
-                        handleDeleteMusician={this.handleDeleteMusician}
-                        firstname={musician.firstName}
-                        lastname={musician.lastName}
-                        genre={musician.genre}
-                        key={musician.lastName.toLowerCase()}
-                        id={musician.firstName.toLowerCase()}
-                        index={index}
-                      />
-                  )}
-              </div>
-            </div>
-          </div>
+      <section className="section">
+      <div className="container">
+      <p className="subtitle is-5">Add and remove musicians using the form below:</p>
+      <br />
+      <div className="columns">
+      <div className="column is-one-third">
+      <form onSubmit={event => this.handleAddMusician(this.state.newmusician.firstName.toLowerCase(), event)}>
+      <div className="field">
+      <div className="control">
+      <input 
+      className="input is-medium"
+      type="text" 
+      placeholder="Enter first name"
+      value={this.state.newmusician.firstName}
+      onChange={this.onAddMusicianFirstNameChange}
+      />
+      </div>
+      </div>
+      <div className="field">
+      <div className="control">
+      <input 
+      className="input is-medium"
+      type="text" 
+      placeholder="Enter last name"
+      value={this.state.newmusician.lastName}
+      onChange={this.onAddMusicianLastNameChange}
+      />
+      </div>
+      </div>
+      <div className="field">
+      <div className="control">
+      <input 
+      className="input is-medium"
+      type="text" 
+      placeholder="Enter genre"
+      value={this.state.newmusician.genre}
+      onChange={this.onAddMusicianGenreChange}
+      />
+      </div>
+      </div>
+      <div className="field">
+      <div className="control">
+      <button type="submit" className="button is-primary is-medium">
+      Add musician
+      </button>
+      </div>
+      </div>
+      </form>
+      </div>
+      <div className="column is-two-thirds">
+      {
+        this.state.musicians.map((musician, index) =>
+        <Musician
+        isAdmin={true}
+        handleUpdateMusician={this.handleUpdateMusician}
+        handleDeleteMusician={this.handleDeleteMusician}
+        firstname={musician.firstName}
+        lastname={musician.lastName}
+        genre={musician.genre}
+        key={musician.lastName.toLowerCase()}
+        id={musician.firstName.toLowerCase()}
+        index={index}
+        />
+        )}
+        </div>
+        </div>
+        </div>
         </section>
-      </Fragment>
-    )
-  }
-}
+        </Fragment>
+        )
+      }
+    }
+    
